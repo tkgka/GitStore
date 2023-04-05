0b9efcb86b33c4b63e81c121ba12ea0b9672a681
tkgka <ksh17171@gmail.com>
2023-04-05 21:22:27 +0900

    Implement [] make MacOS Application File

---
+++ GitStore/GitStore/GitStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitStoreApp.swift
+//  GitStore
+//
+//  Created by 김수환 on 2023/04/05.
+//
+
+import SwiftUI
+
+@main
+struct GitStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
